527227fb6c00b59526a03eb3f4d8b38a59611728
Carlos Severiano <carlossjr@gmail.com>
2018-11-15 18:27:53 -0200

    Multichannel CNN

---
+++ fts2image/MultiChannelFuzzyImageCNN.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Dropout, concatenate
+
+from scipy.ndimage import interpolation
+
+class MultiChannelFuzzyImageCNN:
+
+    def __init__(self, raw_channels, fuzzy_sets, output, nlags=1, steps=1,
+                 conv_layers=1, dense_layers=1, filters=32, kernel_size=3, pooling_size=2, dense_layer_neurons=64, dropout=0):
+        self.nlags = nlags
+        self.steps = steps
+        self.raw_channels = raw_channels
+        self.fuzzysets = fuzzy_sets
+        self.output = output
+        self.conv_layers = conv_layers
+        self.dense_layers = dense_layers
+        self.filters = filters
+        self.kernel_size = [kernel_size,kernel_size]
+        self.dense_layer_neurons = dense_layer_neurons
+        self.pooling = (pooling_size, pooling_size)
+        self.dropout = dropout
+
+    def convert_to_one_hot_image(self, sequence):
+        from keras.utils import to_categorical
+
+        sorted_sequence = sorted(sequence)
+        ind_sequence = [sorted_sequence.index(k) for k in sequence]
+        one_hot_sequence = []
+        for ind in ind_sequence:
+            seq = [0] * self.nlags
+            seq[ind] = 1
+            one_hot_sequence.append(seq)
+
+        return one_hot_sequence
+
+    def series_to_supervised(self, data, n_vars=1, lags=1, steps=1, dropnan=True):
+
+        df = pd.DataFrame(data)
+        cols, names = list(), list()
+        # input sequence (t-n, ... t-1)
+        for i in range(lags, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, steps):
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+        # put it all together
+        agg = pd.concat(cols, axis=1)
+        agg.columns = names
+        # drop rows with NaN values
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg
+
+
+    def design_network(self):
+        # insert proper configs
+        self.model = Sequential()
+
+        for i in np.arange(self.conv_layers):
+            self.model.add(Conv2D(self.filters * (i+1), self.kernel_size, padding="same", activation='relu', input_shape=(self.nlags, self.nlags, len(self.raw_channels)+1)))
+            self.model.add(MaxPooling2D(self.pooling, padding="same"))
+
+        self.model.add(Flatten())
+
+        if self.dropout > 0:
+            self.model.add(Dropout(self.dropout))
+
+        for i in np.arange(self.dense_layers):
+            self.model.add(Dense(self.dense_layer_neurons, activation='relu'))
+
+        self.model.add(Dense(1, activation='linear'))
+        self.model.compile(loss='mse', optimizer='adam')
+
+
+    @staticmethod
+    def plotImage(image):
+        plt.imshow(image, cmap="gray")
+        plt.show()
+
+    def fit(self, train_data, epochs=5, plot_images=False):
+
+        X_images = self.create_images(train_data)
+
+        ## TODO:
+        # Criar imagem one-hot - OK
+        # Definir input shape para CNN
+        # Ajustar funcao de predicao
+        # Observar configuracao do paper Hossein
+        # Se performance fit estiver ruim, testar com dataframe
+
+        if plot_images:
+            for image in X_images:
+                MultiChannelFuzzyImageCNN.plotImage(image[0])
+
+
+        # Create y data
+        y = self.create_output(train_data)
+
+        self.design_network()
+        self.model.fit(X_images, y, epochs)
+
+    def create_output(self, data):
+        sup_data = self.series_to_supervised(data[self.output], lags=self.nlags, steps=self.steps)
+        y = np.array(sup_data.iloc[:, -self.steps].values)
+        return y
+
+    def create_images(self, data):
+        X_images = []
+        # Create X raw channels data
+        for channel in self.raw_channels:
+            sup_data = self.series_to_supervised(data[channel], lags=self.nlags, steps=self.steps)
+
+            X = np.array(sup_data.iloc[:, :self.nlags].values)
+
+            channel_matrix = []
+
+            for sample in X:
+                channel_matrix.append(self.convert_to_one_hot_image(sample))
+
+            X_images.append(channel_matrix)
+        # Create fuzzy image
+        fuzzy_channel_matrix = []
+        sup_data = self.series_to_supervised(data[self.raw_channels[0]], lags=self.nlags, steps=self.steps)
+        X = np.array(sup_data.iloc[:, :self.nlags].values)
+        for sample in X:
+            fuzzy_sequence = []
+            for lag in sample:
+                memberships = [self.fuzzysets[fs].membership(lag) for fs in self.fuzzysets]
+                fuzzy_sequence.append(np.argmax(memberships))
+            fuzzy_channel_matrix.append(self.convert_to_one_hot_image(fuzzy_sequence))
+        X_images.append(fuzzy_channel_matrix)
+        # reshape input values according to network architecture
+        X_images = np.array(X_images)
+        X_images = X_images.reshape(len(data) - (self.nlags + (self.steps - 1)), self.nlags, self.nlags,
+                                    len(self.raw_channels) + 1)
+        return X_images
+
+    def predict(self, test_data):
+        X_images = self.create_images(test_data)
+        return self.model.predict(X_images)
---
+++ tests/test_multichannel_ficnn_forecasting.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import os
+from pyFTS.partitioners import Grid, Entropy, Util as pUtil
+import matplotlib.pyplot as plt
+import math
+from sklearn.metrics import mean_squared_error
+
+from fts2image import MultiChannelFuzzyImageCNN
+
+def normalize(df):
+    mindf = df.min()
+    maxdf = df.max()
+    return (df-mindf)/(maxdf-mindf)
+
+def denormalize(norm_data, original_data):
+    min = original_data.min()
+
+    max = original_data.max()
+
+    return [(n * (max-min)) + min for n in norm_data]
+
+def calculate_rmse(test, forecast, order, step):
+    rmse = math.sqrt(mean_squared_error(test.iloc[(order):], forecast[:-step]))
+    print("RMSE : "+str(rmse))
+    return rmse
+
+def split_data(df, interval):
+    sample_df = df.loc[interval]
+
+    week = (sample_df.index.day - 1) // 7 + 1
+
+    # PARA OS TESTES:
+    # 2 SEMANAS PARA TREINAMENTO
+    train_df = sample_df.loc[week <= 2]
+
+    # 1 SEMANA PARA VALIDACAO
+    validation_df = sample_df.loc[week == 3]
+
+    # 1 SEMANA PARA TESTES
+    test_df = sample_df.loc[week > 3]
+
+    return (train_df, validation_df, test_df)
+
+channel_1 = 'WTG01_Speed'
+channel_2 = 'WTG01_Dir'
+output = 'WTG01_Speed'
+
+df = pd.read_pickle(os.path.join(os.getcwd(), "../data/wind/df_wind_total.pkl"))
+
+#Normalize Data
+
+# Perform Normalization
+norm_df = normalize(df)
+
+# Split data
+interval = ((df.index >= '2017-05') & (df.index <= '2017-06'))
+
+(train_df, validation_df, test_df) = split_data(df, interval)
+(norm_train_df, norm_validation_df, norm_test_df) = split_data(norm_df, interval)
+
+
+def fuzzy_cnn_forecast(train_df, test_df):
+    _conv_layers = 3
+    _dense_layer_neurons = 1280
+    _dense_layers = 5
+    _epochs = 50
+    _filters = 8
+    _kernel_size = 3
+    _npartitions = 100
+    _order = 100
+    _pooling_size = 4
+    _dropout = 0.30
+
+    fuzzy_sets = Grid.GridPartitioner(data=train_df[channel_1].values, npart=_npartitions).sets
+
+    model = MultiChannelFuzzyImageCNN.MultiChannelFuzzyImageCNN([channel_1, channel_2],fuzzy_sets, channel_1, nlags=_order, steps=1,
+                                        conv_layers=_conv_layers, dense_layers=_dense_layers,
+                                        dense_layer_neurons=_dense_layer_neurons, filters=_filters,
+                                        kernel_size=_kernel_size, pooling_size=_pooling_size, dropout=_dropout)
+    model.fit(train_df, epochs=_epochs, plot_images=False)
+
+    forecast = model.predict(test_df)
+
+    return forecast
+
+steps = 1
+
+forecast = fuzzy_cnn_forecast(norm_train_df, norm_test_df)
+forecast = denormalize(forecast, df[output])
+
+_order = 100
+
+forecast.append(0) ## para manter o mesmo tamanho dos demais
+rmse = calculate_rmse(test_df[output], forecast, _order, steps)
+print("RMSE: ", rmse)
+
+plt.figure()
+plt.plot(test_df[output].iloc[_order:600].values)
+plt.plot(forecast[:(600+_order)])
+plt.show()
